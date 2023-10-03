e4628725476307ac615788f990a72a6fcd281e25
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-10-03 15:59:32 +0700

    ensure connetion to google doc using sheety api

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,22 @@
+from api_keys import Sheety_Endpoint, Sheety_Baerer_Token
+import requests
+
+sheety_headers = {
+    "Authorization": f"Bearer {Sheety_Baerer_Token}"
+}
+
+# This class is responsible for talking to the Google Sheet.
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.destination_data = {}
+
+    def read_data_sheet(self):
+        response = requests.get(url=Sheety_Endpoint, headers=sheety_headers)
+        data = response.json()
+        print(data)
+
+    def get_destination_data(self):
+        response = requests.get(url=Sheety_Endpoint, headers=Sheety_Baerer_Token)
+        data = response.json()
+        self.destination_data = data["Lowest Price"]
+        print(data)
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from data_manager import DataManager
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+data_manager = DataManager()
+
+
+data_manager.read_data_sheet()
